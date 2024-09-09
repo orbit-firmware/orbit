d9bde71779c6143b503295b1ea214273a1a8d373
yesitsfebreeze <sayhello@hvlmnns.de>
2024-09-09 20:47:49 +0200

    config preset

--- rmk/macros/src/config.rs
+++ rmk/macros/src/config.rs
@@ -6,7 +6,10 @@ mod read;
 pub fn generate(_input: TokenStream) -> TokenStream {
   let config = read::file("tmp/config.toml");
 
+  let custom_config = read::file("user/config.toml");
+
   println!("Config: {}", config);
+  println!("Config: {}", custom_config);
 
   let generated = quote! {
     use crate::chip::Chip;
