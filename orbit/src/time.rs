01026ad58cc0a1295bf53f58668d2ddacc5b9c2b
yesitsfebreeze <sayhello@hvlmnns.de>
2024-09-15 09:08:29 +0200

    timing

--- orbit/src/time.rs
+++ orbit/src/time.rs
@@ -6,11 +6,23 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[cfg(not(feature = "emulator_enabled"))]
 pub fn now() -> u64 {
-  Instant::now().as_millis() as u64
+  Instant::now().as_micros() as u64
 }
 
 #[cfg(feature = "emulator_enabled")]
 pub fn now() -> u64 {
+  let start = SystemTime::now();
+  let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+  since_the_epoch.as_micros() as u64
+}
+
+#[cfg(not(feature = "emulator_enabled"))]
+pub fn now_ms() -> u64 {
+  Instant::now().as_millis() as u64
+}
+
+#[cfg(feature = "emulator_enabled")]
+pub fn now_ms() -> u64 {
   let start = SystemTime::now();
   let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
   since_the_epoch.as_millis() as u64
@@ -18,4 +30,8 @@ pub fn now() -> u64 {
 
 pub fn elapsed(time: u64) -> u64 {
   now() - time
+}
+
+pub fn elapsed_ms(time: u64) -> u64 {
+  now_ms() - time
 }
